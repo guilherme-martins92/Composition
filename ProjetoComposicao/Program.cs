88cf916f3f44985255aa5ac21a38a8344d650d49
Guilherme Felipe Martins <guilherme.martins92@outlook.com>
2021-04-12 19:30:04 -0300

    Primeira Versao do Projeto

---
+++ ProjetoComposicao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoComposicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
